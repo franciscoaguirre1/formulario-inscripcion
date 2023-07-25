310cd80b403dbe333e30be55a180fa62dc6a0ab7
Francisco Aguirre <franmaguirre@gmail.com>
2023-07-25 16:33:40 -0300

    first commit

---
+++ front/src/app/app.component.ts
@@ -0,0 +1,207 @@
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+
+    title = 'front';
+    formularioEconomíaPopular!: FormGroup
+
+    constructor(private fb: FormBuilder) {
+
+    }
+
+    
+
+    ngOnInit(): void {
+      this.formularioEconomíaPopular = this.fb.group({
+
+        // 1.- CARÁCTER DE LA UNIDAD PRODUCTIVA SOLICITANTE
+        caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+      //   // 2.- DATOS DE IDENTIFICACIÓN        
+      //   //2.1.- Unidad Productiva Unipersonal
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+      //   // 3.- INSCRIPCIONES LEGALES
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+
+      //   // 4.- OBJETO SOCIAL
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+
+      //   // 5.- DOMICILIO PERSONAL O DE LA ASOCIACIÓN FORMAL O INFORMAL
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+      //   // 6.- CONDICIÓN FISCAL
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+      //   // 7.- REPRESENTANTES DE LA ASOCIACIÓN FORMAL O INFORMAL
+
+      //   // primer persona
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+      //   // puede haber más personas
+
+
+
+      //   // 8.- INFORMACIÓN COMPLEMENTARIA
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+
+      //   // 9.- PERSONAS RESPONSABLES DE LA INSCRIPCIÓN
+
+      //   // primer persona
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+      //   // puede haber más personas
+
+
+      //   // Para las personas físicas
+      //   // primer persona
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+      //   // puede haber más personas
+
+
+
+      //   // Situación habitacional
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+      //   caracterUnidadProductivaSolicitante: ['', Validators.required],
+
+
+       })
+      
+    }
+
+
+
+
+    agregarNuevoRepresentante() {
+    
+      const newRowDiv = document.createElement("div");
+      newRowDiv.classList.add("row");
+    
+      for (let i = 0; i < 3; i++) {
+        const colDiv = document.createElement("div");
+        colDiv.classList.add("col-4");
+    
+        const inputGroup = `
+          <div class="mb-3">
+            <label for="input-${i}" class="form-label">Campo ${i + 1}</label>
+            <div class="input-group">
+              <input type="text" class="form-control" id="input-${i}" aria-describedby="basic-addon3 basic-addon4">
+            </div>
+          </div>
+        `;
+    
+        colDiv.innerHTML = inputGroup;
+        newRowDiv.appendChild(colDiv);
+      }
+    
+      const containers = document.getElementsByClassName("container-7");
+      if (containers.length > 0) {
+        containers[0].appendChild(newRowDiv);
+      }
+    }
+
+
+    agregarNuevoResponsable() {
+
+      const newRowDiv = document.createElement("div");
+      newRowDiv.classList.add("row");
+    
+      for (let i = 0; i < 2; i++) {
+        const colDiv = document.createElement("div");
+        colDiv.classList.add("col-6");
+    
+        const inputGroup = `
+          <div class="mb-3">
+            <label for="input-${i}" class="form-label">Campo ${i + 1}</label>
+            <div class="input-group">
+              <input type="text" class="form-control" id="input-${i}" aria-describedby="basic-addon3 basic-addon4">
+            </div>
+          </div>
+        `;
+    
+        colDiv.innerHTML = inputGroup;
+        newRowDiv.appendChild(colDiv);
+      }
+    
+      const containers = document.getElementsByClassName("container-9");
+      if (containers.length > 0) {
+        containers[0].appendChild(newRowDiv);
+      }
+
+
+
+    }
+
+
+
+
+
+
+
+}
